ceb683c2d5ec56adce2f2e074c20f81fba9ed644
Kristen Mazza <kbmazza@gmail.com>
2023-01-08 13:46:06 -0800

    Populate display when numbers are pressed

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+// Calculator operator functions
 const add = function(a, b) {
     return a + b;
 };
@@ -14,6 +15,8 @@ const divide = function(a, b) {
     return a / b;
 };
 
+// Operate function takes on an operator and two numbers, then calls the addition
+// subtraction, multiplication, and division functions on them
 function operate(operator, a, b) {
     switch (operator) {
         case addSign:
@@ -30,3 +33,21 @@ function operate(operator, a, b) {
             break;
     }
 }
+
+
+const buttons = document.querySelectorAll('.buttons');
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const value = button.getAttribute("value");
+        const display = document.querySelector('.display');
+
+        // Populate the display when numbers are selected
+        while (button.classList.contains('numbers')) {
+            display.innerText += value;
+            displayValue = display.innerText;
+            console.log(displayValue);
+            return displayValue;
+        }
+    })
+})                                                                                                                                                                                                                         
+
