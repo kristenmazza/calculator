79a70d8c7b2e235134fc26e43139e5173a1f3b72
Kristen Mazza <kbmazza@gmail.com>
2023-01-15 12:17:59 -0800

    Perform calculator operations

--- javascript.js
+++ javascript.js
@@ -1,53 +1,78 @@
 // Calculator operator functions
-const add = function(a, b) {
-    return a + b;
-};
-
-const subtract = function(a, b) {
-    return a - b;
-};
-
-const multiply = function(a, b) {
-    return a * b;
-};
-
-const divide = function(a, b) {
-    return a / b;
-};
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
 
 // Operate function takes on an operator and two numbers, then calls the addition
 // subtraction, multiplication, and division functions on them
 function operate(operator, a, b) {
+    let result = null;
     switch (operator) {
-        case addSign:
-            add(a, b);
+        case "addition":
+            result = add(a, b);
             break;
-        case subtractSign:
-            subtract(a, b);
+        case "subtraction":
+            result = subtract(a, b);
             break;
-        case multiplySign(a, b):
-            multiply(a, b);
+        case "multiplication":
+            result = multiply(a, b);
             break;
-        case divideSign(a, b):
-            divide(a, b);
+        case "division":
+            result = divide(a, b);
             break;
+        case "equal":
+            result = a;
     }
+    return result;
 }
 
+let displayValue = "";
+let calculatedValue = null;
+let secondValue = null;
+let operatorValue = null;
+const display = document.querySelector('.display');
 
-const buttons = document.querySelectorAll('.buttons');
-buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        const value = button.getAttribute("value");
-        const display = document.querySelector('.display');
+let operatorLastSelected = false;
+const numbers = document.querySelectorAll('.numbers');
+numbers.forEach(number => {
+    number.addEventListener("click", () => {
+
+        if (operatorLastSelected === true) {
+            // reset display value
+            display.innerText = "";
+
+            operatorLastSelected = false;
+        }
+
+        const value = number.getAttribute("value");
 
         // Populate the display when numbers are selected
-        while (button.classList.contains('numbers')) {
-            display.innerText += value;
-            displayValue = display.innerText;
-            console.log(displayValue);
-            return displayValue;
-        }
-    })
+        display.innerText += value;
+        displayValue = parseFloat(display.innerText);
+    });
 })                                                                                                                                                                                                                         
+
+const operators = document.querySelectorAll('.operators');
+operators.forEach(operator => {
+    operator.addEventListener("click", () => {
+
+        const previousOperator = operatorValue;
+        operatorValue = operator.getAttribute("value");
+
+        if (previousOperator === "equal" && operatorValue === "equal") {
+            return;
+        }
+
+        if (calculatedValue === null) {
+            calculatedValue = displayValue;
+        } else {
+            calculatedValue = operate(previousOperator, calculatedValue, displayValue);
+            display.innerText = calculatedValue;
+        }
+
+        operatorLastSelected = true;
+    });
+});
+                                                                                                                                                                                              
 
